57d3095552372dfc0b6b283afeb1ea07dcd97c4b
Alex Hien <alex.hien@raja.fr>
2022-06-27 19:04:11 +0200

    Initiation to Thymeleaf

--- src/main/java/com/springboot/demo/myapp/rest/DemoRestController.java
+++ src/main/java/com/springboot/demo/myapp/rest/DemoRestController.java
@@ -1,37 +1,19 @@
 package com.springboot.demo.myapp.rest;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@RestController
+@Controller
 public class DemoRestController {
 
-    @Value("${coach.name}")
-    private String coachName;
-
-    @Value("${coach.team}")
-    private String coachTeam;
-
-    @GetMapping("/teaminfo")
-    public String getTeamInfo() {
-        return "Hello " + coachName + " he is in " + coachTeam;
-    }
-
-    @GetMapping("/")
-    public String sayHelloWorld() {
-        return "Hello world : " + LocalDateTime.now() + " auto updated";
-    }
-
-    @GetMapping("/workout")
-    public String getWorkout() {
-        return "let's azeazeaego for workoaeazezaeaezaut sds!";
-    }
-
-    @GetMapping("/test")
-    public String getTest() {
-        return "let's aa for aa azaa!";
-    }
+  @GetMapping("/hello")
+    public String sayHello(Model m) {
+      m.addAttribute("theDate", new java.util.Date());
+      return "helloworld";
+  }
 }
